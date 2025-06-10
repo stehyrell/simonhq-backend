53a48269f7d10c5ad8acb2eb67da1f8f824af780
Simon Tehyrell <simontehyrell@MacBookAir.lan>
2025-06-10 23:06:08 +0200

    fix: include OAuth2 client credentials

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ const auth = new google.auth.OAuth2(
   process.env.GMAIL_CLIENT_SECRET
 );
 auth.setCredentials({
-  refresh_token: process.env.GMAIL_REFRESH_TOKEN
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+  access_token: process.env.GMAIL_ACCESS_TOKEN,
+  scope: 'https://mail.google.com/'
 });
+
 const gmail = google.gmail({ version: 'v1', auth });
 
 // === OpenAI setup ===
